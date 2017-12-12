398f787e2b4c0879b7895d72ef229c33e5b9ce68
Cristofer [XPS] <cristofer.dev@gmail.com>
2017-12-11 23:52:35 -0300

    ADD btn RT and favorites
     Changes to be committed:
    	modified:   src/components/Main/index.jsx
    	modified:   src/components/Message/index.jsx
    	modified:   src/components/Message/message.css
    	modified:   src/components/MessageList/index.jsx

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -6,9 +6,10 @@ import InputText from '../InputText'
 import ProfileBar from '../ProfileBar'
 
 class Main extends Component {
-    constructor () {
-        super()
+    constructor (props) {
+        super(props)
         this.state = {
+            user: Object.assign({}, this.props.user, {retweets:[]},{favorites:[]}),
             OpenText : false,
             messages: [
                 { 
@@ -17,7 +18,10 @@ class Main extends Component {
                     picture: 'https://randomuser.me/api/portraits/women/17.jpg',
                     displayName : 'May Lao',
                     username : 'maylao',
-                    date : Date.now()
+                    date : Date.now(),
+                    retweets: 0,
+                    favorites: 0
+
                 },
                 {
                     id: uuid.v4(),
@@ -25,13 +29,17 @@ class Main extends Component {
                     picture: 'https://randomuser.me/api/portraits/women/72.jpg',
                     displayName: 'Lia Tomson',
                     username: 'Liatom',
-                    date: Date.now()
+                    date: Date.now(),
+                    retweets: 0,
+                    favorites: 0
                 }
             ]
         }
         this.handleSendText = this.handleSendText.bind(this)
         this.handleCloseText = this.handleCloseText.bind(this)
         this.handleOpenText = this.handleOpenText.bind(this)
+        this.handleRetweet = this.handleRetweet.bind(this)
+        this.handleFavorite = this.handleFavorite.bind(this)
     }
 
     handleSendText (event) {
@@ -59,6 +67,50 @@ class Main extends Component {
         this.setState({ OpenText : true })
     }
 
+
+    handleRetweet (msgId) {        
+        let alreadyRetweeted = this.state.user.retweets.filter( rt => rt === msgId)
+
+        if(alreadyRetweeted.length === 0){            
+            let messages = this.state.messages.map(msg => {
+                if(msg.id === msgId){
+                    msg.retweets++
+                }
+                return msg
+            })
+
+            let user = Object.assign({}, this.state.user)
+            user.retweets.push(msgId)
+
+            this.setState({
+                messages: messages,
+                user: user
+            })
+        }
+
+    }
+
+    handleFavorite (msgId) {
+        let alreadyFavorited = this.state.user.favorites.filter( fav => fav === msgId)
+    
+        if(alreadyFavorited.length === 0){
+            let messages = this.state.messages.map(msg =>{
+                if(msg.id === msgId){
+                    msg.favorites++
+                }
+                return msg
+            })
+
+            let user = Object.assign({}, this.state.user)
+            user.favorites.push(msgId)
+
+            this.setState({
+                messages: messages,
+                user: user
+            })
+        }
+}
+
     renderOpenText (){
 
         if ( this.state.OpenText ) {
@@ -80,7 +132,11 @@ class Main extends Component {
                     onOpenText={this.handleOpenText}
                 />
                 {this.renderOpenText()}
-                <MessageList messages={this.state.messages} />
+                <MessageList 
+                    messages={this.state.messages} 
+                    onRetweet={this.handleRetweet}
+                    onFavorite={this.handleFavorite}
+                />
             </div>
         )
     }
--- src/components/Message/index.jsx
+++ src/components/Message/index.jsx
@@ -5,6 +5,28 @@ import styles from './message.css'
 class Message extends Component {
     constructor (props) {
         super(props)
+
+        this.state = {
+            pressFavorite: false,
+            pressRetweet: false
+        }
+
+        this.onPressRetweet = this.onPressRetweet.bind(this)
+        this.onPressFavorite = this.onPressFavorite.bind(this)
+    }
+
+    onPressFavorite(){
+        this.props.onFavorite();
+        this.setState({
+            pressFavorite: true
+        })
+    }
+
+    onPressRetweet(){
+        this.props.onRetweet();
+        this.setState({
+            pressRetweet: true
+        })
     }
 
     render () {
@@ -26,8 +48,20 @@ class Message extends Component {
 
                 <div className={styles.buttons}>
                     <span className={styles.icon}><span className="fa fa-reply"></span></span>
-                    <span className={styles.icon}><span className="fa fa-retweet"></span></span>
-                    <span className={styles.icon}><span className="fa fa-star"></span></span>
+                    <span 
+                        className={ (this.state.pressRetweet) ? styles.rtGreen : '' }
+                        onClick={this.onPressRetweet}
+                    >
+                        <span className="fa fa-retweet"></span>
+                        <span className={styles.num}>{this.props.numRetweets}</span>
+                    </span>
+                    <span 
+                        className={ (this.state.pressFavorite) ? styles.favYellow : '' }
+                        onClick={this.onPressFavorite}
+                    >
+                        <span className="fa fa-star"></span>
+                        <span className={styles.num}>{this.props.numFavorites}</span>
+                    </span>
                 </div>
 
             </div>
