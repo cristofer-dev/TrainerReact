b6b9727d6d93f9f067dc47c18ef0c730515343c8
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-09 00:06:23 -0300

    MODIFY Comoponent InputText to StateLess
     Changes to be committed:
    	modified:   src/components/InputText/index.jsx

--- src/components/InputText/index.jsx
+++ src/components/InputText/index.jsx
@@ -1,21 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import styles from './input-text.css'
 
-class InputText extends Component {
-	render () {
-		return (
-			<form className={styles.form} onSubmit={this.props.onSendText}>
-				<textarea className={styles.text} name='text'>
-					{(this.props.userNameToReply) ? `@${this.props.userNameToReply}` : ''}
-					
-				</textarea>
-				<div className={styles.buttons}>
-					<button className={styles.close} onClick={this.props.onCloseText}>Cerrar</button>
-					<button className={styles.send} type='submit'>Enviar</button>
-				</div>
 
-			</form>
-		)
-	}
+function InputText(props) {
+  return (
+    <form className={styles.form} onSubmit={props.onSendText}>
+      <textarea className={styles.text} name='text'>
+        {(props.userNameToReply) ? `@${props.userNameToReply}` : ''}
+      </textarea>
+      <div className={styles.buttons}>
+        <button className={styles.close} onClick={props.onCloseText}>Cerrar</button>
+        <button className={styles.send} type='submit'>Enviar</button>
+      </div>
+
+    </form>
+  )
 }
+
 export default InputText
