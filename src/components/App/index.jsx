85b4ad3b4cb7cd870004fd67f85b1e5414dee1ea
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-17 01:13:16 -0300

    ADD React-RouterV4
     Changes to be committed:
    	modified:   package.json
    	modified:   src/components/App/index.jsx
    	modified:   src/index.jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,9 +1,32 @@
 import React, { Component } from 'react'
+import { Link, Route, Switch } from 'react-router-dom';
+
 import 'normalize-css'
 
 import styles from './app.css'
 import Header from '../Header'
 import Main from '../Main'
+
+/* Home component */
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+)
+
+/* Category component */
+const Category = () => (
+  <div>
+    <h2>Category</h2>
+  </div>
+)
+
+/* Products component */
+const Products = () => (
+  <div>
+    <h2>Products</h2>
+  </div>
+)
 
 class App extends Component {
 	constructor (){
@@ -18,15 +41,29 @@ class App extends Component {
 		}
 	}
 
-    render() {
-        return (
-            <div>
-                <Header/>
+  render() {
+    return (
+      <div>
+        <Header/>
 
-                <Main user={this.state.user}/>
-            </div>
-        )
-    }
+        <Main user={this.state.user}/>
+
+        <nav className="navbar navbar-light">
+          <ul className="nav navbar-nav">
+
+            <li><Link to="/">Homes</Link></li>
+            <li><Link to="/category">Category</Link></li>
+            <li><Link to="/products">Products</Link></li>
+
+          </ul>
+        </nav>
+
+        <Route path="/" component={Home}/>
+        <Route path="/category" component={Category}/>
+        <Route path="/products" component={Products}/>
+      </div>
+      )
+  }
 }
 
 export default App 
