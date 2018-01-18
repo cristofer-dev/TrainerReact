b8fcf12263eddeaabd9ab121cb6dcdee3e619e3e
Cristofer [HP] <cristofer.dev@gmail.com>
2018-01-17 23:47:51 -0300

    ADD React-route v4
     Changes to be committed:
    	modified:   src/components/App/index.jsx
    	modified:   src/components/Header/index.jsx

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -7,21 +7,13 @@ import styles from './app.css'
 import Header from '../Header'
 import Main from '../Main'
 
-/* Home component */
-const Home = () => (
+
+const Profile = () => (
   <div>
-    <h2>Home</h2>
+    <h2>Profile</h2>
   </div>
 )
 
-/* Category component */
-const Category = () => (
-  <div>
-    <h2>Category</h2>
-  </div>
-)
-
-/* Products component */
 const Products = () => (
   <div>
     <h2>Products</h2>
@@ -45,22 +37,14 @@ class App extends Component {
     return (
       <div>
         <Header/>
-
-        <Main user={this.state.user}/>
-
-        <nav className="navbar navbar-light">
-          <ul className="nav navbar-nav">
-
-            <li><Link to="/">Homes</Link></li>
-            <li><Link to="/category">Category</Link></li>
-            <li><Link to="/products">Products</Link></li>
-
-          </ul>
-        </nav>
-
-        <Route path="/" component={Home}/>
-        <Route path="/category" component={Category}/>
-        <Route path="/products" component={Products}/>
+          <Route
+            exact path="/"
+            render={() => (
+              <Main user={this.state.user}/>
+            )}
+          />
+          <Route path="/profile" component={Profile}/>
+          <Route path="/products" component={Products}/>
       </div>
       )
   }
